e65a527d2de0ea7e67fb73f867b195bc135c69f7
mohamedazizchebil <chebilaziz461@gmail.com>
2025-11-29 18:27:29 +0100

    Initial project setup

---
+++ backend/CarRentalPlatformBackend/src/main/java/com/poly/carrentalplatformbackend/CarRentalPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.poly.carrentalplatformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalPlatformBackendApplication.class, args);
+	}
+
+}
