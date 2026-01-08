c3f5618b5c9729a9013e180c7b497fcd8ea2f4ab
mohamedazizchebil <chebilaziz461@gmail.com>
2026-01-08 03:30:01 +0100

    changes

--- frontend/car-rental-frontend/src/pages/Home.jsx
+++ frontend/car-rental-frontend/src/pages/Home.jsx
@@ -1,4 +1,66 @@
-// src/pages/admin/CategoriesAdmin.jsx
+import { Link } from "react-router-dom";
+import Footer from "../components/common/Footer";
+
 export default function Home() {
-    return <div>Page Admin Catégories</div>;
+  return (
+    <>
+      {/* ===== NAVBAR ===== */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+        <div className="container">
+          <Link className="navbar-brand fw-bold text-primary" to="/">
+            PremiumCar
+          </Link>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#mainNavbar"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+<div className="d-flex align-items-center ms-auto" id="mainNavbar">
+  <Link to="/login" className="btn btn-outline-primary me-2">
+    Se connecter
+  </Link>
+
+  <Link to="/register" className="btn btn-primary">
+    Créer un compte
+  </Link>
+</div>
+
+
+        </div>
+      </nav>
+
+      {/* ===== HERO / CONTENT ===== */}
+      <div className="container py-5">
+        <div className="row align-items-center">
+          <div className="col-md-6">
+            <h1 className="fw-bold mb-3">
+              Location de voitures <span className="text-primary">simple & rapide</span>
+            </h1>
+            <p className="text-muted fs-5">
+              Découvrez nos voitures disponibles, réservez en quelques clics
+              et payez en toute sécurité.
+            </p>
+
+            <div className="d-flex gap-3 mt-4">
+              <Link to="/login" className="btn btn-primary btn-lg">
+                Voir les voitures Disponibles
+              </Link>
+              <Link to="/register" className="btn btn-outline-secondary btn-lg">
+                Commencer
+              </Link>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      {/* ===== FOOTER ===== */}
+      <Footer/>
+    </>
+  );
 }
